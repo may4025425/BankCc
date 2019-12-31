dbb6da2f580e4c1482a7505ec9cca130b4fad1ab
may4025425 <may4025425@gmail.com>
2019-12-31 16:30:38 +0800

    12/31

--- app/src/main/java/com/example/cc/LoginActivity.java
+++ app/src/main/java/com/example/cc/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.example.cc;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EdgeEffect;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void login(View view){
+        EditText edusername = findViewById(R.id.username);
+        EditText edpassword = findViewById(R.id.password);
+        String username = edusername.getText().toString();
+        String password = edpassword.getText().toString();
+        //省成兩行
+
+        if("may".equals(username) && "1234".equals(password)) {
+            Toast.makeText(this,"登入成功",Toast.LENGTH_LONG).show();
+            finish();
+        }else {
+            new AlertDialog.Builder(this)
+                    .setTitle("登入訊息")
+                    .setMessage("登入失敗")
+                    .setPositiveButton("ok",null)
+                    .show();
+        }
     }
 }
--- app/src/main/java/com/example/cc/MainActivity.java
+++ app/src/main/java/com/example/cc/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.cc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean logon = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(!logon){
+            Intent login = new Intent(this,LoginActivity.class);
+        }else{
+
+        }
     }
 }
