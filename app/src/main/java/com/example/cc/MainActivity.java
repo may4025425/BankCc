d663881d342a96055ca36d5c0c5d5a886c9f1989
may4025425 <may4025425@gmail.com>
2020-01-06 07:08:30 +0800

    1/6

--- app/src/main/java/com/example/cc/MainActivity.java
+++ app/src/main/java/com/example/cc/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -49,5 +50,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+    public void ok(View view){
+        Intent back = new Intent(this,MoneyActivity.class);
+        startActivity(back);
     }
 }
